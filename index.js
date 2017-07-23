88d403384ae9d4d76783029222574cfc31bea33e
kshivanku <kshivanku@gmail.com>
2017-07-23 12:50:25 -0700

    getting the movie

--- index.js
+++ index.js
@@ -30,6 +30,17 @@ restService.post('/hook', function(req, res) {
   function askingMovie(app) {
     var date_period = app.getArgument("date-period");
     var genre = app.getArgument("genre");
+    var year = date_period.slice(0,4);
+    var url = "https://api.themoviedb.org/3/discover/movie?api_key=cd4cb70f5659be258a39908dd671ee1f&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&release_date.lte=" + year;
+    if(genre != null) {
+      url = url + "&with_genres=" + genre;
+    }
+    request({
+      uri: url,
+      method: 'GET'
+    }, function(err, response, body){
+      console.log(body);
+    });
     app.ask("getting movie from " + date_period + " from the genre " + genre);
   }
 
