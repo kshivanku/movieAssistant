9adefc19e9ea35094d7016381a5480a36738bce6
kshivanku <kshivanku@gmail.com>
2017-07-22 20:40:19 -0700

    testing the hook

---
+++ index.js
@@ -0,0 +1,18 @@
+//NODE PACKAGES
+const express = require('express');
+const bodyParser = require('body-parser');
+const ApiAiApp = require('actions-on-google').ApiAiApp;
+const request = require('request');
+
+//SETTING UP SERVICE
+const restService = express();
+restService.use(bodyParser.json());
+restService.listen((process.env.PORT || 5000), function() {
+  console.log("Server listening");
+});
+
+//WEBHOOK Requests
+restService.post('/hook', function(req, res) {
+  console.log("request received");
+  console.log(req);
+}
